677c83a45aa19f46e52a0ed92e462e6f97231a72
jonathan <jonathanfagan@pursuit.org>
2021-01-12 13:56:28 -0500

    refactored and changed variable names

--- Stack/ValidParens.js
+++ Stack/ValidParens.js
@@ -1,21 +1,21 @@
 var isValid = function(s) {
     // if string is empty return true
     if ( s === null || s.length <= 0 ) return true;
-    //split string
-    arr = s.split('')
+    
     // create a "stack"
     stack = []
+    
     //loop through string 
-    for(let  i of arr){
-        if (i === '[') stack.push(']')
-        else if (i === '{') stack.push('}')
-        else if( i === '(') stack.push(')')
-        else if( stack.length === 0 || i !== stack.pop()) return false
+    for(let  char of s){
+        if (char === '[') stack.push(']')
+        else if (char === '{') stack.push('}')
+        else if( char === '(') stack.push(')')
+        else if( stack.length === 0 || char !== stack.pop()) return false
     }
     if (stack.length === 0) return true;
     return false;
 };
 
-let string = "(])"
+let string = "([])"
 
 console.log(isValid(string))
