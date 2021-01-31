491c83b5f111e03a2412c79bc460456e6b358e66
jonathan <jonathanfagan@pursuit.org>
2021-01-31 12:47:19 -0500

    implemented changes

---
+++ BinarySearchTrees/BinaryTree.js
@@ -0,0 +1,121 @@
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    Insert(val) {
+
+        let newNode = new Node(val);
+        if (!this.root) {
+            return this.root = newNode;
+        }
+
+
+        let current = this.root;
+        // console.log("line 14");
+        while (true) {
+            if (val === current.value) return null;
+
+            if (val < current.value) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
+                } else {
+                    current = current.left;
+                }
+            }
+            else if (val > current.value) {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                } else {
+                    current = current.right;
+                }
+            }
+
+        }
+
+    }
+
+    Find(value) {
+
+        // console.log('line 41:', "node:", start, 'value:', valueArg)
+
+        if (!this.root) return "no root";
+        let found = false;
+
+        function recursive(bst) {
+            if (value === bst.value) {
+                console.log("found it")
+                found = true;
+            }
+
+            if (bst.left !== null && value < bst.value) {
+                recursive(bst.left);
+            }
+
+            if (bst.right !== null && value > bst.value) {
+                recursive(bst.right);
+            }
+        }
+        recursive(this.root)
+        return found;
+    }
+
+    Contain(value){
+
+        if(!this.root) return false;
+        let found = false;
+        let current = this.root;
+
+        while(!found && current !== null){
+            if(value < current.value){
+                if(current.left !== null){
+                    current = current.left;
+                }
+            }else if(value > current.value){
+                if(current.right !== null){
+                    current = current.right;
+                }
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+var tree = new BinarySearchTree();
+
+
+tree.Insert(7);
+//console.log(tree);
+tree.Insert(5);
+//console.log(tree);
+tree.Insert(8);
+//console.log(tree);
+tree.Insert(4);
+//console.log(tree);
+tree.Insert(6);
+//console.log(tree);
+console.log(tree.Contain(6))
